e3891df484fbb4a75f71ab20fd0215a22985af73
Amy Boudsady <aeimskei@gmail.com>
2017-12-18 13:39:39 -0800

    Grouping options for more details

--- suggest.php
+++ suggest.php
@@ -74,6 +74,121 @@ include("inc/header.php"); ?>
           <td><input type="text" id="email" name="email" /></td>
         </tr>
         <tr>
+          <th><label for="category">Category</label></th>
+          <td><select id="category" name="category">
+            <option value="">Select One</option>
+            <option value="Books">Book</option>
+            <option value="Movies">Movie</option>
+            <option value="Music">Music</option>
+          </select></td>
+        </tr>
+        <tr>
+          <th><label for="title">Title</label></th>
+          <td><input type="text" id="title" name="title"></td>
+        </tr>
+        <tr>
+          <th><label for="format">Format</label></th>
+          <td><select id="format" name="format">
+            <option value="">Select One</option>
+            <optgroup label="Books">
+              <option value="Audio">Audio</option>
+              <option value="Ebook">Ebook</option>
+              <option value="Hardback">Hardback</option>
+              <option value="Paperback">Paackperb</option>
+            </optgroup>
+            <optgroup label="Movies">
+              <option value="Blu-ray">Blu-ray</option>
+              <option value="DVD">DVD</option>
+              <option value="Streaming">Streaming</option>
+              <option value="VHS">VHS</option>
+            </optgroup>
+            <optgroup label="Music">
+              <option value="Cassette">Cassette</option>
+              <option value="CD">CD</option>
+              <option value="MP3">MP3</option>
+              <option value="Vinyl">Vinly</option>
+            </optgroup>
+          </select></td>
+        </tr>
+        <tr>
+          <th>
+              <label for="genre">Genre</label>
+          </th>
+          <td>
+            <select name="genre" id="genre">
+              <option value="">Select One</option>
+              <optgroup label="Books">
+                <option value="Action">Action</option>
+                <option value="Adventure">Adventure</option>
+                <option value="Comedy">Comedy</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Historical">Historical</option>
+                <option value="Historical Fiction">Historical Fiction</option>
+                <option value="Horror">Horror</option>
+                <option value="Magical Realism">Magical Realism</option>
+                <option value="Mystery">Mystery</option>
+                <option value="Paranoid">Paranoid</option>
+                <option value="Philosophical">Philosophical</option>
+                <option value="Political">Political</option>
+                <option value="Romance">Romance</option>
+                <option value="Saga">Saga</option>
+                <option value="Satire">Satire</option>
+                <option value="Sci-Fi">Sci-Fi</option>
+                <option value="Tech">Tech</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Urban">Urban</option>
+              </optgroup>
+              <optgroup label="Movies">
+                <option value="Action">Action</option>
+                <option value="Adventure">Adventure</option>
+                <option value="Animation">Animation</option>
+                <option value="Biography">Biography</option>
+                <option value="Comedy">Comedy</option>
+                <option value="Crime">Crime</option>
+                <option value="Documentary">Documentary</option>
+                <option value="Drama">Drama</option>
+                <option value="Family">Family</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Film-Noir">Film-Noir</option>
+                <option value="History">History</option>
+                <option value="Horror">Horror</option>
+                <option value="Musical">Musical</option>
+                <option value="Mystery">Mystery</option>
+                <option value="Romance">Romance</option>
+                <option value="Sci-Fi">Sci-Fi</option>
+                <option value="Sport">Sport</option>
+                <option value="Thriller">Thriller</option>
+                <option value="War">War</option>
+                <option value="Western">Western</option>
+              </optgroup>
+              <optgroup label="Music">
+                <option value="Alternative">Alternative</option>
+                <option value="Blues">Blues</option>
+                <option value="Classical">Classical</option>
+                <option value="Country">Country</option>
+                <option value="Dance">Dance</option>
+                <option value="Easy Listening">Easy Listening</option>
+                <option value="Electronic">Electronic</option>
+                <option value="Folk">Folk</option>
+                <option value="Hip Hop/Rap">Hip Hop/Rap</option>
+                <option value="Inspirational/Gospel">Insirational/Gospel</option>
+                <option value="Jazz">Jazz</option>
+                <option value="Latin">Latin</option>
+                <option value="New Age">New Age</option>
+                <option value="Opera">Opera</option>
+                <option value="Pop">Pop</option>
+                <option value="R&B/Soul">R&amp;B/Soul</option>
+                <option value="Reggae">Reggae</option>
+                <option value="Rock">Rock</option>
+              </optgroup>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <th><label for="email">Year</label></th>
+          <td><input type="text" id="year" name="year" /></td>
+        </tr>
+        <tr>
           <th><label for="details">Suggest Item Details</label></th>
           <td><textarea name="details" id="details"></textarea></td>
         </tr>
