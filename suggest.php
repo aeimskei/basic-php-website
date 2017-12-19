64e69e6b95d83486e3985f569924693480b4b030
Amy Boudsady <aeimskei@gmail.com>
2017-12-18 16:31:26 -0800

    Display the Error Message

--- suggest.php
+++ suggest.php
@@ -61,22 +61,29 @@ include("inc/header.php"); ?>
 <div class="section page">
   <div class="wrapper">
     <h1>Suggest a Media Item</h1>
-    <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
-      echo "<p>Thanks for the email! I&rsquo;ll check out your sugestion shortly!</p>";
-     } else { ?>
-    <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+      <?php 
+        if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+          echo "<p>Thanks for the email! I&rsquo;ll check out your sugestion shortly!</p>";
+        } else { 
+          if (isset($error_message)) {
+            echo "<p class='message'>".$error_message . "</p>";
+          } else {
+            echo "<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>";
+          }
+        }
+      ?>
     <form method="post" action="suggest.php">
       <table>
         <tr>
-          <th><label for="name">Name</label></th>
+          <th><label for="name">Name (required)</label></th>
           <td><input type="text" id="name" name="name" /></td>
         </tr>
         <tr>
-          <th><label for="email">Email</label></th>
+          <th><label for="email">Email (required)</label></th>
           <td><input type="text" id="email" name="email" /></td>
         </tr>
         <tr>
-          <th><label for="category">Category</label></th>
+          <th><label for="category">Category (required)</label></th>
           <td><select id="category" name="category">
             <option value="">Select One</option>
             <option value="Books">Book</option>
@@ -85,7 +92,7 @@ include("inc/header.php"); ?>
           </select></td>
         </tr>
         <tr>
-          <th><label for="title">Title</label></th>
+          <th><label for="title">Title (required)</label></th>
           <td><input type="text" id="title" name="title"></td>
         </tr>
         <tr>
@@ -202,7 +209,6 @@ include("inc/header.php"); ?>
       </table>
       <input type="submit" value="Send" />
     </form>
-    <?php } ?>
    </div>
 </div>
 
